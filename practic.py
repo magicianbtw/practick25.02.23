62bcdaa0518850c2303826eb3e9504d9167f9a48
magicianbtw <124551932+magicianbtw@users.noreply.github.com>
2023-06-16 19:22:05 +0300

    Add files via upload

---
+++ practic.py
@@ -0,0 +1,18 @@
+class Alphabet:
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = letters
+
+    def print(self):
+        print(self.letters)
+
+    def letters_num(self):
+        return len(self.letters)
+
+
+
+eng_alphabet = Alphabet("English",
+                        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                         't', 'u', 'v', 'w', 'x', 'y', 'z'])
+eng_alphabet.print()
+print(eng_alphabet.letters_num())  
